8202c983909a53b995ce1931f99a29061b6747f0
Barbara <barbara.erdec.golovic@gmail.com>
2026-02-03 18:32:02 +0100

    Implementation of frist method test

---
+++ Prime.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers
+{
+    public class Prime
+    {
+        public void PrvaMetoda()
+        {
+            Console.WriteLine("Ovo je prva metoda u klasi Prime.");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
-﻿Console.WriteLine("Dobar dan, Ivan Kako ste!");
+﻿using PrimeNumbers;
+
+Console.WriteLine("Dobar dan, Ivan Kako ste!");
 
 // Implementirajte ispis prvih N brojeva koji se unose kao vparametar konzolne aplikacije.
+
+// poziva metedoe
+new Prime().PrvaMetoda();
